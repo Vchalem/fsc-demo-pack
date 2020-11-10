bb9b40df0dbbbf1e21a7be0e0b503e589448a10b
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-11-10 11:50:00 -0500

    Update orgINit.sh to get ready for presentation

--- orgInit.sh
+++ orgInit.sh
@@ -4,10 +4,35 @@
 sfdx force:org:create -f config/project-scratch-def.json -a FSCADK2 --setdefaultusername -d 7
 
 #pckg installs
-sfdx force:package:install --package 04t1E000000cmtN -w 20 
+sfdx force:package:install --package 04t1E000000jazHQAQ -w 20 
 #FSC Extn
 #Has all fieldsets for Lightning pages like Financial Account tab on Account
 sfdx force:package:install --package 04t1E000001Iql5 -w 20
+
+
+sfdx force:source:push 
+
+sfdx force:user:permset:assign -n FinancialServicesCloudStandard
+#This permission set is for data load as some permissions are not assigned yet
+#You can remove this after data load
+#Data load instructions are in dataLoad.sh
+
+#sfdx force:user:permset:assign -n FSC_DataLoad_Custom
+#sfdx sfdmu:run --sourceusername csvfile --targetusername FSCADK2 -p data/sfdmu/ --noprompt
+
+
+
+#Send user password reset email
+sfdx force:apex:execute -f config/setup.apex
+
+
+
+sfdx force:org:open
+
+
+
+
+
 #FSC Extn Commercial Banking* 
 #Requires more dashboards
 #sfdx force:package:install --package 04t80000000lTrZ -w 20
@@ -17,21 +42,3 @@ sfdx force:package:install --package 04t1E000001Iql5 -w 20
 #sfdx force:package:install --package 04t80000000lTqH -w 20
 #FSC Lightning Flow Templates
 #sfdx force:package:install --package 04t3i000000jP1g -w 20
-
-
-sfdx force:source:push 
-
-sfdx force:user:permset:assign -n FinancialServicesCloudStandard
-#This permission set is for data load as some permissions are not assigned yet
-#You can remove this after data load
-#Data load instructions are in orgInitAndData.sh
-#sfdx force:user:permset:assign -n FSC_DataLoad_Custom
-
-
-
-#Send user password reset email
-sfdx force:apex:execute -f config/setup.apex
-
-
-sfdx force:org:open
-
