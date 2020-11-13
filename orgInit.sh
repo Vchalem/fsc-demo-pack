4f9a7888a22924a3859c62b3ca2af2b548f44a86
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-11-13 16:06:38 -0500

    Enable SFDMU for testing

--- orgInit.sh
+++ orgInit.sh
@@ -20,7 +20,7 @@ sfdx force:user:permset:assign -n FinancialServicesCloudStandard
 #Data load instructions are in dataLoad.sh
 
 sfdx force:user:permset:assign -n FSC_DataLoad_Custom
-sfdx sfdmu:run --sourceusername csvfile --targetusername FSCADK2 -p data/sfdmu/ --noprompt
+#sfdx sfdmu:run --sourceusername csvfile --targetusername FSCADK2 -p data/sfdmu/ --noprompt
 sfdx sfdmu:run --sourceusername csvfile  -p data/sfdmu/ --noprompt
 
 
